9c282b3c55489deb775a9f95a5e628c41416f8c2
Gustavo Marques <gugamello2014@yahoo.com>
2024-11-09 13:45:55 -0300

    refactor: simplify typography sizes by removing lineHeight from constants

--- src/constants/style/typography.ts
+++ src/constants/style/typography.ts
@@ -3,27 +3,21 @@ import plugin from "tailwindcss/plugin";
 const sizes = {
   14: {
     fontSize: "0.875rem",
-    lineHeight: "1.25rem",
   },
   16: {
     fontSize: "1rem",
-    lineHeight: "1.5rem",
   },
   18: {
     fontSize: "1.125rem",
-    lineHeight: "1.625rem",
   },
   20: {
     fontSize: "1.25rem",
-    lineHeight: "1.75rem",
   },
   24: {
     fontSize: "1.5rem",
-    lineHeight: "2rem",
   },
   32: {
     fontSize: "2rem",
-    lineHeight: "2.625rem",
   },
 };
 
@@ -35,12 +29,11 @@ const weights = {
 
 export const typographyPlugin = plugin(function ({ addComponents }) {
   const comps = Object.entries(weights).map(([weight, value]) => {
-    return Object.entries(sizes).map(([size, { fontSize, lineHeight }]) => {
+    return Object.entries(sizes).map(([size, props]) => {
       return {
         [`.t-${weight}-${size}`]: {
           fontWeight: value,
-          fontSize,
-          lineHeight,
+          ...props,
         },
       };
     });
