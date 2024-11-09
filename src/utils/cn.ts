a26cb49fea377d584e536c40567c4d59a2a6f944
Gustavo Marques <gugamello2014@yahoo.com>
2024-11-09 13:26:34 -0300

    refactor: update cn param type

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,7 +1,7 @@
-import type { ClassNameValue } from 'tailwind-merge';
-import { twMerge } from 'tailwind-merge';
+import type { ClassList } from "@builder.io/qwik";
+import type { ClassNameValue } from "tailwind-merge";
+import { twMerge } from "tailwind-merge";
 
-
-export function cn(...inputs: ClassNameValue[]) {
-  return twMerge(inputs)
+export function cn(...inputs: (ClassNameValue | ClassList)[]) {
+  return twMerge(inputs as ClassNameValue[]);
 }
