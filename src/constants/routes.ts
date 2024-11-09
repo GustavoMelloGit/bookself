1bef494e2da1171af028618ed48e537cdce15154
Gustavo Marques <gugamello2014@yahoo.com>
2024-11-09 18:58:47 -0300

    feat: add collection page

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,4 +2,5 @@ export const Routes = {
   home: "/",
   collection: (id: string) => `/collection/${id}`,
   section: (id: string) => `/section/${id}`,
+  createCollection: "/create-collection",
 };
---
+++ src/routes/collection/[id]/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+  return (
+    <div>
+      <h1>Coleção</h1>
+    </div>
+  );
+});
