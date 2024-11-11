0083757a6a819fb973d7f5dbbad86bda59d9bee5
Gustavo Marques <gugamello2014@yahoo.com>
2024-11-11 11:05:26 -0300

    feat: add DocumentHead for improved SEO with meta tags for BookSelf tierlist creation and sharing platform

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import type { QRL } from "@builder.io/qwik";
 import { $, component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
 import { routeLoader$, useNavigate } from "@builder.io/qwik-city";
 import type { SubmitHandler } from "@modular-forms/qwik";
 import { useForm, valiForm$, type InitialValues } from "@modular-forms/qwik";
@@ -75,3 +76,77 @@ export default component$(() => {
     </div>
   );
 });
+
+export const head: DocumentHead = {
+  title: "BookSelf | Crie e Compartilhe Tierlists de Livros",
+  meta: [
+    {
+      name: "description",
+      content:
+        "No BookSelf, você pode criar e compartilhar tierlists de livros em diversos temas. Organize e classifique suas leituras favoritas, de romances a ficção e mais!",
+    },
+    {
+      name: "keywords",
+      content:
+        "BookSelf, tierlist de livros, classificação de livros, opiniões de livros, lista de livros, recomendação de livros, temas de livros",
+    },
+    {
+      name: "author",
+      content: "BookSelf",
+    },
+    {
+      name: "language",
+      content: "pt-BR",
+    },
+    {
+      property: "og:title",
+      content: "BookSelf | Crie e Compartilhe Tierlists de Livros",
+    },
+    {
+      property: "og:type",
+      content: "website",
+    },
+    {
+      property: "og:url",
+      content: "https://www.bookself.com.br",
+    },
+    {
+      property: "og:description",
+      content:
+        "Crie e compartilhe tierlists de livros com o BookSelf! Organize e classifique suas leituras favoritas, compartilhe sua opinião e inspire outros leitores.",
+    },
+    {
+      property: "og:image",
+      content: "https://www.bookself.com.br/imagens/bookself-banner.png",
+    },
+    {
+      property: "og:site_name",
+      content: "BookSelf",
+    },
+    {
+      property: "og:locale",
+      content: "pt_BR",
+    },
+    {
+      name: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      name: "twitter:title",
+      content: "BookSelf | Crie e Compartilhe Tierlists de Livros",
+    },
+    {
+      name: "twitter:description",
+      content:
+        "No BookSelf, você pode criar e compartilhar tierlists de livros em diversos temas. Organize suas leituras, classifique seus favoritos e compartilhe suas opiniões!",
+    },
+    {
+      name: "twitter:image",
+      content: "https://www.bookself.com.br/imagens/bookself-banner.png",
+    },
+    {
+      name: "twitter:site",
+      content: "@bookself",
+    },
+  ],
+};
